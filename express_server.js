0ad89c9c2b05cc138a5662ad096a1807711c20d1
Dexter Chan <dexterchann@gmail.com>
2020-03-16 21:36:24 +0000

    Created new url form page

--- express_server.js
+++ express_server.js
@@ -13,9 +13,14 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
