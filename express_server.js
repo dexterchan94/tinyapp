5677dade6b2035f53b7d01e663d95d2a75ae0ef7
Dexter Chan <dexterchann@gmail.com>
2020-03-16 21:07:24 +0000

    Added show url template and route

--- express_server.js
+++ express_server.js
@@ -18,8 +18,9 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
 });
 
 app.get("/hello", (req, res) => {
